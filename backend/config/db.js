f2f425df143945ab90a62a398dd001be771ffcaa
Prasad Kakde <kakdeprasad11@gmail.com>
2026-03-05 12:37:22 +0530

    db connection solve

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,18 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
   if (err) {
-    console.error(" Database connection failed:", err.message);
+    console.error("Database connection failed:", err);
   } else {
-    console.log(" MySQL Database Connected");
+    console.log("MySQL Connected");
   }
 });
 
