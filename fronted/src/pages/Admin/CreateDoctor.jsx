52ebc7c602bec12b174c63e5d705cdd32ff0db70
Prasad Kakde <kakdeprasad11@gmail.com>
2026-03-06 22:23:55 +0530

    api import problem

--- fronted/src/pages/Admin/CreateDoctor.jsx
+++ fronted/src/pages/Admin/CreateDoctor.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import DashboardNavbar from "../../components/DashboardNavbar";
-import { API } from "../../api";
+import { API } from "../../api.js";
 
 const CreateDoctor = () => {
   const [form, setForm] = useState({
