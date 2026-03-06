71c6358a6b9347cb306713e62afbc5c0f0788b06
Prasad Kakde <kakdeprasad11@gmail.com>
2026-03-06 23:20:05 +0530

    fix vercel

--- fronted/src/pages/Admin/AdminDashboard.jsx
+++ fronted/src/pages/Admin/AdminDashboard.jsx
@@ -3,13 +3,7 @@ import axios from "axios";
 import DashboardNavbar from "../../components/DashboardNavbar";
 import DashboardCard from "../../components/DashboardCard";
 import StatCard from "../../components/StatCard";
-
-
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "https://medi-vault-dg6d.onrender.com/api"
-});
+import { API } from "../../api.js";
 
 const AdminDashboard = () => {
 
