cf2daff3a5d095d9de5320019a8655fd3d56c851
Prasad Kakde <kakdeprasad11@gmail.com>
2026-03-07 07:11:58 +0530

    login fix

--- fronted/src/pages/Register.jsx
+++ fronted/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { API } from "../api";
+//import { API } from "../api";
 
 export default function Register() {
   const navigate = useNavigate();
