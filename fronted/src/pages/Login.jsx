cdf2a20e9ead6a0e1daa6a96fd1663e4446bc0c3
Prasad Kakde <kakdeprasad11@gmail.com>
2026-03-16 22:30:33 +0530

    login

--- fronted/src/pages/Login.jsx
+++ fronted/src/pages/Login.jsx
@@ -22,11 +22,14 @@ export default function Login() {
 
       let endpoint = "";
 
-      if (role === "admin") endpoint = `https://medi-vault-wf7o.onrender.com/api/admin/login `;
+      if (role === "admin")
+       endpoint = "https://medi-vault-wf7o.onrender.com/api/admin/login";
 
-      if (role === "doctor") endpoint = `https://medi-vault-zmx6.onrender.com/api/doctors/login`;
+      if (role === "doctor")
+       endpoint = "https://medi-vault-zmx6.onrender.com/api/doctors/login";
 
-      if (role === "patient") endpoint = `https://medi-vault-zmx6.onrender.com/api/patients/login`;
+      if (role === "patient")
+       endpoint = "https://medi-vault-zmx6.onrender.com/api/patients/login";
 
       const res = await axios.post(endpoint, {
         email,
