f53599a1b23e86c1a51601346f594cb44f27a50d
Prasad Kakde <kakdeprasad11@gmail.com>
2026-03-17 09:00:29 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import adminRoutes from "./routes/adminRoutes.js";
 import doctorRoutes from "./routes/doctorRoutes.js";
 import patientRoutes from "./routes/patientRoutes.js";
 import appointmentRoutes from "./routes/appointmentRoutes.js";
-
-//import recordRoutes from "./routes/recordRoute.js"
-
 import authRoutes from "./routes/authRoutes.js";
 
 
+dotenv.config();
 
-import cors from "cors";
+
+const app = express();
+
+
 app.use(cors({
   origin: [
     "http://localhost:5173",
@@ -21,40 +24,25 @@ app.use(cors({
   credentials: true
 }));
 
-dotenv.config();             
 
-     
-app.use(express.json());      
+app.use(express.json());
+
 
 app.use("/api/auth", authRoutes);
-
-app.use("/api/admin", adminRoutes);  //http://localhost:5000/api/admin/login
-
-app.use("/api/doctors", doctorRoutes); //POST http://localhost:5000/api/doctors
-
-app.use("/api/patients", patientRoutes);  //http://localhost:5000/api/patients
-
-app.use("/api/appointments", appointmentRoutes); //http://localhost:5000/api/appointments
-
-
-
-//app.use("/api/records", recordRoutes);
-
-
-
 app.use("/api/admin", adminRoutes);
+app.use("/api/doctors", doctorRoutes);
+app.use("/api/patients", patientRoutes);
+app.use("/api/appointments", appointmentRoutes);
 
-
-
-
-
-
-const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("MediVault Backend Running");
 });
 
+
+const PORT = process.env.PORT || 5000;
+
+
 app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
